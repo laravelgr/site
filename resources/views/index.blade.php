4179bb8726e6d64ee4aecdc1f793183ba2e71765
george <george@drakakis.gr>
2019-03-19 23:09:08 +0200

    Init commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Greek community</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    <img src="img/laravel_logo.png" alt="Laravel Greek community logo">
+                </div>
+                <div class="links">
+                    <a href="http://slack.laravel.gr" target="_blank">Join Chat</a>
+                    <a href="/blog" target="_blank">Blog</a>
+                    <a href="https://www.youtube.com/channel/UCXgd0om3ZNufW8GocLUH23Q/videos" target="_blank">Youtube</a>
+                    <a href="https://www.twitter.com/laravelgr" target="_blank">Twitter</a>
+                    <a href="https://www.facebook.com/laravelgr" target="_blank">Facebook</a>
+                    <a href="https://www.meetup.com/athens-laravel-meetup" target="_blank">Meetup</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

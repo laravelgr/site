0cfb3bded127f5bc0e1c85ed62c4fe47f3d91712
George Drakakis <george@drakakis.gr>
2019-11-03 02:34:08 +0200

    signup btn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,41 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel Greek community</title>
-        <link rel="stylesheet" href="{{url('/css/app.css')}}">
-    </head>
-    <body>
-        <div class="container mx-auto">
-            <div class="flex justify-center p-5">
-                <img class="mt-6" src="img/laravel_logo.png" alt="Laravel Greek community logo">
-            </div>
-            <div class="flex justify-center socials text-center p-5">
-                <a class="px-4 py-2 m-2" href="http://slack.laravel.gr" target="_blank"><img src="/img/icons/slack.svg" alt="Slack page"></a>
-                <a class="px-4 py-2 m-2" href="https://www.youtube.com/channel/UCXgd0om3ZNufW8GocLUH23Q/videos" target="_blank"><img src="/img/icons/youtube.svg" alt="Youtube page"></a>
-                <a class="px-4 py-2 m-2" href="https://www.facebook.com/laravelgr" target="_blank"><img src="/img/icons/facebook.svg" alt="Facebook page">
-                </a>
-                <a class="px-4 py-2 m-2" href="https://www.twitter.com/laravelgr" target="_blank"><img src="/img/icons/twitter.svg" alt="Twitter page">
-                </a>
-            </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel Greek community</title>
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{url('/css/app.css')}}">
+</head>
+<body>
+<div class="container mx-auto">
+    <div class="flex justify-center  text-center p-5 mt-10">
+        <div class="border-b-2 ">
+            <h1 class="laravel-title text-blue-800">Laravel</h1>
+            <h4 class="laravel-subtitle">The Greek community for laravel framework</h4>
         </div>
-    </body>
+
+    </div>
+    <div class="flex justify-center socials text-center p-5">
+        <a class="px-4 py-2 m-2" href="http://slack.laravel.gr" target="_blank"><img src="/img/icons/slack.svg"
+                                                                                     alt="Slack page"></a>
+        <a class="px-4 py-2 m-2" href="https://www.youtube.com/channel/UCXgd0om3ZNufW8GocLUH23Q/videos" target="_blank"><img
+                    src="/img/icons/youtube.svg" alt="Youtube page"></a>
+        <a class="px-4 py-2 m-2" href="https://www.facebook.com/laravelgr" target="_blank"><img
+                    src="/img/icons/facebook.svg" alt="Facebook page">
+        </a>
+        <a class="px-4 py-2 m-2" href="https://www.twitter.com/laravelgr" target="_blank"><img
+                    src="/img/icons/twitter.svg" alt="Twitter page">
+        </a>
+    </div>
+</div>
+<div class="flex text-center mt-20 p-10 bg-blue-100">
+    <div class="flex-1 text-blue-800">
+        Δεν έχεις λογαριασμό; Δημιούργησε έναν τώρα!
+        <button class="ml-6 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+            Εγγραφή
+        </button>
+    </div>
+</div>
+</body>
 </html>
